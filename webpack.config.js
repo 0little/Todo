4078506d1ff70031c2e5b7e761c21d8bf2b3fa0f
0little <1667883363@qq.com>
2018-04-07 14:16:36 +0800

    these files have changed

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 const path = require('path');
 const webpack = require('webpack');
 const HTMLPlugin = require('html-webpack-plugin');
+const ExtractPlugin = require('extract-text-webpack-plugin');
 const isDev = process.env.NODE_ENV === 'development';
 
 const config = {
     target: 'web',
     entry: path.join(__dirname, './src/index.js'),
     output: {
-        filename: 'bundle.js',
+        filename: 'bundle.[hash:8].js',
         path: path.join(__dirname, './dist')
     },
     module: {
@@ -23,20 +24,6 @@ const config = {
                     'style-loader',
                     'css-loader'
 
-                ]
-            },
-            {
-                test: /\.styl$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    },
-                    'stylus-loader'
                 ]
             },
             {
@@ -74,6 +61,20 @@ const config = {
 module.exports = config;
 
 if(isDev) {
+    config.module.rules.push({
+        test: /\.styl$/,
+        use: [
+            'style-loader',
+            'css-loader',
+            {
+                loader: 'postcss-loader',
+                options: {
+                    sourceMap: true
+                }
+            },
+            'stylus-loader'
+        ]
+    });
     config.devServer = {
         port: 8000,
         host: '0.0.0.0',
@@ -87,4 +88,30 @@ if(isDev) {
         new webpack.NoEmitOnErrorsPlugin()
     );
     config.devtool = '#cheap-module-eval-source-map';
+} else {
+    config.output.filename = '[name].[chunkhash:8].js';
+
+    config.module.rules.push(
+        {
+            test: /\.styl$/,
+            use: ExtractPlugin.extract({
+                fallback: 'style-loader',
+                use: [
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    'stylus-loader'
+                ]
+            })
+        },
+    );
+
+    config.plugins.push(
+        new ExtractPlugin('styles.css')
+    );
+
 }
